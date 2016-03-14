0e8caa2bd4cffbd6c0133271b6ee840db2a95157
LaxmiManosca23 <laxmi.manosca@f706-21-34147.bmcc.cuny.edu>
2016-03-14 12:56:16 -0400

    Work Done in last class

--- Js/work.js
+++ Js/work.js
@@ -1,13 +1,13 @@
-$(document).ready(function (){
+$(document).ready(function () {
     console.log("js loaded!");
 
-    $('.hide-title img').mouseenter(function (){
+    $('.hide-title img').mouseenter(function () {
         $(this).siblings().animate({
             opacity: "1",
             right:"50px"
         });
     });
-    $('.hide-title img').mouseleave(function (){
+    $('.hide-title img').mouseleave(function () {
         $(this).siblings().animate({
             opacity: "0",
             right:"0"
@@ -15,3 +15,4 @@ $(document).ready(function (){
     });
 
 });
+
